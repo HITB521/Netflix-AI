800498f5692effdcd19e558fce3c69ae0d97639c
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2024-01-27 01:20:01 +0530

    changes Brought onauthstate change to header . bug fixing

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,15 +2,13 @@ import React, { useEffect } from 'react'
 import Login from './Login';
 import Browse from './Browse';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { auth } from '../utils/firebase';
-import { onAuthStateChanged } from 'firebase/auth';
-import { useDispatch } from 'react-redux';
-import { addUser, removeUser } from '../utils/userSlice';
+
+
 
 
 
 const Body = () => {
-  const dispatch =useDispatch();
+ 
     const appRouter = createBrowserRouter([
         {
         path:"/",
@@ -21,20 +19,7 @@ const Body = () => {
             element:<Browse/>
         },
     ]);
-    useEffect(()=>{
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          
-          const { uid,email,displayName,photoUrl} = user;
-          dispatch(addUser({uid:uid,email:email,displayName:displayName,photoUrl:photoUrl}));
-          // ...
-        } else {
-          dispatch(removeUser());
-        }
-      });
-
-
-    },[]);
+    
   return (
     <div>
       <RouterProvider router={appRouter}/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { LOGO, USER_AVATAR } from '../utils/constants'
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
-import { useSelector } from 'react-redux';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user= useSelector(store=>store.user);
   
   const handleSignOut=()=>{
     signOut(auth).then(() => {
-     navigate("/");
+     
     }).catch((error) => {
       navigate("/error");
     });
 
   };
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        
+        const { uid,email,displayName,photoUrl} = user;
+        dispatch(addUser({uid:uid,email:email,displayName:displayName,photoUrl:photoUrl}));
+        navigate("/browse");  // without authentication nobody can access the browse page
+        // ...
+      } else {
+        dispatch(removeUser());// without authentication nobody can access the browse page
+        navigate("/");
+      }
+    });
+
+
+  },[]);
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
       <img className='w-44' src={LOGO} />
