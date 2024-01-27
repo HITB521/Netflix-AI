23ea4ffbbebc0537e5adea05085466b4141e8899
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2024-01-27 19:16:12 +0530

    Main qw

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,6 +39,7 @@ const Header = () => {
   },[]);
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
+      {/* commit */}
       <img className='w-44' src={LOGO} />
        {user && <div className='flex p-2' >
         <img className="w-12 h-12 "src={USER_AVATAR} alt ="">
